06702dcb71ed45c3ce37452831baf83ace7aa790
DraGBlaze <frendidacosta@gmail.com>
2026-01-28 00:22:57 +0700

    Menabahkan get bagian kas

--- Backend/Controllers/Kas.js
+++ Backend/Controllers/Kas.js
@@ -1,4 +1,49 @@
 import Kas from "../Models/ModelKas.js";
+import Users from "../Models/ModelUser.js";
 
 
-export const getKas = async (req, res) => {}
+export const getKas = async (req, res) => {
+    try {
+
+        const role = req.role;
+        const userId = req.userId;
+
+        if(role == "anggota") {
+            return res.status(403).json({ msg: "Akses ditolak"})
+        }
+
+        const response = await Kas.findAll({
+            include: [{
+                model: Users,
+                attributes: ['nama', 'role']
+            }]
+        });
+
+        res.status(200).json(response);
+
+    } catch (error) {
+        res.status(500).json({ msg: "Terjadi kesalahan"})
+    }
+}
+
+export const getKasMy = async (req, res) => {
+    try {
+
+        const role = req.role;
+        const userId = req.userId;
+
+        const response = await Kas.findAll(
+            {where: { userId: req.userId}},
+            {
+            include: [{
+                model: Users,
+                attributes: ['nama', 'role']
+            }]
+        });
+
+        res.status(200).json(response);
+
+    } catch (error) {
+        res.status(500).json({ msg: "Terjadi kesalahan"})
+    }
+}
